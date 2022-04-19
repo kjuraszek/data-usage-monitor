1ac91c9109b9664bb35adf16f39357b94a1715eb
kjuraszek <donkrissocorleone@gmail.com>
2022-04-19 13:10:03 +0200

    Update application __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,18 @@
+"""
+This module creates a Flask application instance using Application Facotry pattern.
+"""
 from flask import Flask
 
 
 def init_app():
+    """
+    This function initializes Flask application.
+    """
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.ProductionConfig' if app.config['ENV'] == 'production'
-                            else 'config.DevelopmentConfig')
+                           else 'config.DevelopmentConfig')
 
     with app.app_context():
-        from . import routes
+        from . import routes  # noqa: F401 pylint: disable=C0415,W0611
 
         return app
