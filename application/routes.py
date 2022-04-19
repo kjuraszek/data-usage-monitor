4c57e3af305224b3daf13e6ddfd554cb46a85c80
kjuraszek <donkrissocorleone@gmail.com>
2022-04-19 13:24:01 +0200

    Update routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,17 @@
+"""
+Flask Application routing.
+"""
+import json
 from flask import current_app as app
 from flask import render_template
-import json
+
 
 @app.route('/', methods=['GET'])
 def index():
-    with open('data.json', 'r') as data_file:
+    """
+    Function serves a template for a home page.
+    """
+    with open('data.json', 'r', encoding='utf8') as data_file:
         data = dict(json.load(data_file))
         data_usage = data.get('data_usage', 0)
     return render_template('home.html', data_usage=data_usage)
