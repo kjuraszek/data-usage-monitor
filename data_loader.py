8614ba6c51de504e9d7835efa6130d701520d92e
kjuraszek <donkrissocorleone@gmail.com>
2022-04-25 21:00:54 +0200

    Update data-loader

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,15 @@
 """
 Data Loader module is used to gather the data from Huawei router API.
 """
+import datetime
 import time
-import json
-from decimal import Decimal
 import requests
 import bs4
 import schedule
 
-API_ENDPOINT = 'http://192.168.8.1/api/monitoring/month_statistics'
+ROUTER_API_ENDPOINT = 'http://192.168.8.1/api/monitoring/month_statistics'
+FLASK_API_ENDPOINT = 'http://127.0.0.1:5000/api/usage-stamp'
+JOB_SCHEDULE = 10
 
 
 def job():
@@ -16,18 +17,21 @@ def job():
     A function which is scheduled within specified schedule.
     This function collects usage data from API and saves it to a JSON file.
     """
-    response = requests.get(API_ENDPOINT)
-    response.raise_for_status()
-    soup = bs4.BeautifulSoup(response.content, 'lxml')
-    current_dl = int(soup.find('currentmonthdownload').get_text())
-    current_ul = int(soup.find('currentmonthupload').get_text())
-    data_usage = float(round(Decimal((current_dl + current_ul) / (1024**3)), 3))
-    print('Current usage:', data_usage)
-    with open('data.json', 'w', encoding='utf8') as data_file:
-        json.dump({'data_usage': data_usage}, data_file, ensure_ascii=False, indent=4)
+    router_api_response = requests.get(ROUTER_API_ENDPOINT)
+    router_api_response.raise_for_status()
+    soup = bs4.BeautifulSoup(router_api_response.content, 'lxml')
+    current_month_download = int(soup.find('currentmonthdownload').get_text())
+    current_month_upload = int(soup.find('currentmonthupload').get_text())
+    time_stamp = datetime.datetime.now(datetime.timezone.utc)
+    usage_stamp_data = {'current_month_download': current_month_download,
+                        'current_month_upload': current_month_upload,
+                        'time_stamp': time_stamp}
+    flask_api_response = requests.put(FLASK_API_ENDPOINT, params=usage_stamp_data)
+    flask_api_response.raise_for_status()
 
 
-schedule.every(1).minutes.do(job)
+
+schedule.every(JOB_SCHEDULE).minutes.do(job)
 
 while True:
     schedule.run_pending()
