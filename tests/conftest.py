da67eea91f064b66bf4aba2ba0ae882dbf7fc83d
Krzysztof <34898047+kjuraszek@users.noreply.github.com>
2022-05-30 20:57:53 +0200

    Add tests (#14)
    - added `testing` target in Makefile
    - added tests for blueprints

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+"""
+Set of fixtures for application.
+"""
+import pytest
+from application import init_app
+
+
+@pytest.fixture()
+def app():
+    app = init_app()
+    app.config.update({
+        'TESTING': True,
+        'PRESERVE_CONTEXT_ON_EXCEPTION': False
+    })
+
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
