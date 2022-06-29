3b60c505f40085bbb2b50c83944809f8d5473910
kjuraszek <donkrissocorleone@gmail.com>
2022-06-30 01:11:18 +0200

    Exclude dist folder from jest coverage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   collectCoverage: true,
-  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**", "!**.config.js", "!**/coverage/**"],
+  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**", "!**.config.js", "!**/coverage/**", "!**/dist/**"],
   setupFilesAfterEnv: ['<rootDir>/tests/unit/setup.js']
 }
